3db6d80caf84aa942299cf6af143f8889461106d
〘 sᴀᴛʜᴀɴɪᴄ 〙 <sathanic498@gmail.com>
2025-09-17 20:17:25 +0530

    〘 Z I Y A - M D 〙

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   prefix: process.env.PREFIX || '.',
   ownerNumber: process.env.OWNER_NUMBER || '918921016567',
   sessionPath: process.env.SESSION_PATH || './sessions/ziya-auth',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8000,
   watchMode: process.env.WATCH_MODE === 'true'
 };
