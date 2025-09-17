78d99a6535e2996e5a5cf782cc7f47a671ec387c
〘 sᴀᴛʜᴀɴɪᴄ 〙 <sathanic498@gmail.com>
2025-09-17 20:16:23 +0530

    〘 Z I Y A - M D 〙

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 let start = Date.now();
 
 app.get('/', (req, res) => res.send('Ziya is running'));
